2ffedf69a1b2047b11d75d453d5f90c9ac87b9ee
QuickGnu <awilkinson19@choate.edu>
2018-09-10 14:58:40 -0400

    One line!

--- Command_lines.py
+++ Command_lines.py
@@ -2,6 +2,4 @@ import sys
 
 # Got help from: https://stackoverflow.com/questions/70797/user-input-and-command-line-arguments
 
-user_input = sys.argv
-path = user_input.pop(0)
-x = [print("Hello, "+i+"!") for i in user_input]
+x = [print("Hello, "+i+"!") for i in sys.argv[1:]]
