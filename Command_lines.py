cbde966bbec848c6957ec2af3870bdae9d3c631b
QuickGnu <awilkinson19@choate.edu>
2018-09-12 08:07:36 -0400

    comments

--- Command_lines.py
+++ Command_lines.py
@@ -1,5 +1,11 @@
 import sys
-
 # Got help from: https://stackoverflow.com/questions/70797/user-input-and-command-line-arguments
-
+'''
+1: What is the value at position 0?
+Position 0 is the first item
+2: How can you tell how many arguments have been passed?
+Use the function len()
+3: What happens if you pass more arguments or fewer arguments than the program expects?
+it returns an error
+'''
 x = [print("Hello, "+i+"!") for i in sys.argv[1:]]
